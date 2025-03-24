40c6a3f41afa99386ca96a05e52364587078d969
Carol <162912256+carolmarques09@users.noreply.github.com>
2025-03-24 15:20:20 -0300

    Add files via upload

--- MedicoController.java
+++ MedicoController.java
@@ -1,16 +1,27 @@
 package med.voll.api.controler;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Scanner;
+Scanner repository = null;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 
     @PostMapping
-    public void cadastrar(@RequestBody String json) {
-        System.out.println(json);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        System.out.println(dados);
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }
+
+    public MedicoController() {
     }
 }
