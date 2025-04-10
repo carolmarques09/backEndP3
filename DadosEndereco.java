5179bd2684b2415e37a2dc0c4dacad1ade91ac6f
Carol <162912256+carolmarques09@users.noreply.github.com>
2025-04-10 15:37:49 -0300

    Update DadosEndereco.java

--- DadosEndereco.java
+++ DadosEndereco.java
@@ -2,3 +2,17 @@ package med.voll.api.endereco;
 
 public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) {
 }
+
+// Pendente: fazer os getters, setters e construtores
+
+public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+  if (dados.nome() != null) {
+    this.nome = dados.nome();
+  }
+  if (dados.telefone() != null) {
+    this.telefone = dados.telefone();
+  }
+  if (dados.endereco() != null) {
+    this.endereco.atualizarInformacoes(dados.endereco());
+  }
+}
